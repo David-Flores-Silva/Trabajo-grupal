210e818a79e04dc044dc00bab35c70b3aa4a4a8e
David-Flores-Silva <68410195+David-Flores-Silva@users.noreply.github.com>
2020-08-04 07:55:39 -0500

    Update and rename FuncionAdvanced.java to ZfuncionAdvanced.java

--- Advanced-example/ZfuncionAdvanced.java
+++ Advanced-example/ZfuncionAdvanced.java
@@ -4,11 +4,11 @@ public class FuncionAdvanced {
 
 	public static void main(String[] args) {
 			
-		Campo LaptopCnew = new Ventilador (new Soporte(new Lapto()));
+		Acampo LaptopCnew = new Ventilador (new Soporte(new Lapto()));
 		Mostrar(LaptopCnew);
 	}
 	
-	static void Mostrar(Campo componente) {
+	static void Mostrar(Acampo componente) {
 		System.out.println(componente.name() );
 		System.out.println(componente.price());
 		System.out.println("----------");
